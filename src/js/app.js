91f062978dde2c60de4f1d058d7f1dc63898ca89
unknown <rorchenko@gmail.com>
2022-09-14 20:48:09 +0300

    toggle js

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,15 @@
-const toggle = document.querySelector('#toggle');
+const toggles = document.querySelectorAll('input[name="toggle"]');
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('change', () => {
+        if (toggle.value === 'yearly') {
+            document.querySelector('#starter').textContent = '12$';
+            document.querySelector('#professional').textContent = '36$';
+            document.querySelector('#company').textContent = '56$';
+        } else {
+            document.querySelector('#starter').textContent = '19$';
+            document.querySelector('#professional').textContent = '54$';
+            document.querySelector('#company').textContent = '89$';
+        }
+    });
+});
